3f40dd12f34883c2a7a85fba36e322016a12b707
Emmanuel Briney <emmanuel.briney@docker.com>
2017-12-28 16:58:37 +0100

    quickfix for Docker for windows
    Change default docker host in docker client vendor
    Signed-off-by: ebriney <emmanuel.briney@docker.com>

--- vendor/github.com/docker/docker/client/client_windows.go
+++ vendor/github.com/docker/docker/client/client_windows.go
@@ -1,4 +1,4 @@
 package client
 
 // DefaultDockerHost defines os specific default if DOCKER_HOST is unset
-const DefaultDockerHost = "npipe:////./pipe/docker_engine"
+const DefaultDockerHost = "npipe:////./pipe/docker_engine_windows"
